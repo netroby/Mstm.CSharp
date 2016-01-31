36db10163d25b6278882dcde97cd6da2161f65e0
mstm2014 <476256857@qq.com>
2016-01-31 19:12:23 +0800

    修改测试

--- RedPacket/Mstm.RedPacket.Tests/RedPacketProviderTests.cs
+++ RedPacket/Mstm.RedPacket.Tests/RedPacketProviderTests.cs
@@ -26,14 +26,61 @@ namespace Mstm.RedPacket.Tests
 
         private void GetOneRedPacketTest()
         {
-            decimal amount = 300 * 300 + 21;
-            //100 * 81 + 17  有3.32  小于最小值3.33
-            int packageCount = 100 * 15 + 16;
-            decimal ceiling = 21.45M;
-            decimal floor = 30;
+            Func<RedPacketConfig> func = new Func<RedPacketConfig>(GetRedPacketConfig);
+            var config = func.Invoke();
             int errorCount = 0;
+            List<decimal> errorList = new List<decimal>();
+
+            //均分红包
+            decimal avgPkg = Math.Round(config.Amount / config.PacketCount, 2);
+            //最大红包
+            decimal maxPkg = Math.Round(avgPkg * (100 + config.Ceiling) / 100, 2);
+            //最小红包
+            decimal minPkg = Math.Round(avgPkg * (100 - config.Floor) / 100, 2);
+
+
+            while (true)
+            {
+                var money = RedPacketProvider.GetOneRedPacket(func);
+                if (money <= 0) { break; }
+                if (money > maxPkg || money < minPkg)
+                {
+                    errorList.Add(money);
+                    errorCount++;
+                }
+                config.CurrentAmount += money;
+                config.CurrentPackageCount++;
+                System.Console.WriteLine(money + "");
+            }
+
+            Assert.AreEqual(config.Amount, config.CurrentAmount);
+            Assert.IsTrue(errorCount == 0);
+        }
+
+
+
+
+        /// <summary>
+        /// 获取红包配置
+        /// </summary>
+        /// <returns></returns>
+        RedPacketConfig GetRedPacketConfig()
+        {
+            //当前已发红包总金额
             decimal currentAmount = 0;
+            //当前已发红包总数
             int currentPackageCount = 0;
+
+
+            //总金额
+            decimal amount = 300 * 300 + 100;
+            //100 * 81 + 17  有3.32  小于最小值3.33
+            //总红包数
+            int packageCount = 250;
+            //上限
+            decimal ceiling = 20;
+            //下限
+            decimal floor = 30;
             //均分红包
             decimal avgPkg = Math.Round(amount / packageCount, 2);
             //最大红包
@@ -41,24 +88,18 @@ namespace Mstm.RedPacket.Tests
             //最小红包
             decimal minPkg = Math.Round(avgPkg * (100 - floor) / 100, 2);
 
-            List<decimal> errorList = new List<decimal>();
-
-            while (true)
+            RedPacketConfig config = new RedPacketConfig()
             {
-                var money = RedPacketProvider.GetOneRedPacket(amount, currentAmount, packageCount, currentPackageCount, ceiling, floor);
-                if (money <= 0) { break; }
-                if (money > maxPkg || money < minPkg)
-                {
-                    errorList.Add(money);
-                    errorCount++;
-                }
-                currentAmount += money;
-                currentPackageCount++;
-                System.Console.WriteLine(money + "");
-            }
+                Amount = amount,
+                PacketCount = packageCount,
+                CurrentAmount = currentAmount,
+                CurrentPackageCount = currentPackageCount,
+                Ceiling = ceiling,
+                Floor = floor
+            };
+            return config;
 
-            Assert.AreEqual(amount, currentAmount);
-            Assert.IsTrue(errorCount == 0);
+
         }
     }
 }
