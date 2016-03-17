688bf8d6361a87d3a43b6db0bc00cafb00c3b069
mstm2014 <476256857@qq.com>
2016-03-17 17:12:53 +0800

    测试构建带统计的简单where语句

--- SQLAnalysis/Mstm.SQLAnalysis.UnitTests/MySQLAnalysisProviderTests.cs
+++ SQLAnalysis/Mstm.SQLAnalysis.UnitTests/MySQLAnalysisProviderTests.cs
@@ -31,93 +31,9 @@ namespace Mstm.SQLAnalysis.UnitTests
         }
 
 
-        [TestMethod]
-        public void BuildNormalWhereTest()
-        {
-            var list = new List<NormalFilterInfo>() { 
-            new NormalFilterInfo(){
-                FieldName="UserName",
-                FieldType= FieldTypeEnum.Text,
-                ConnectRelation= ConnectRelationEnum.Or,
-                NormalWhereRelation= NormalWhereRelationEnum.LikePrefix,
-                WhereValue="张"
-            },
-            new NormalFilterInfo(){
-                FieldName="UserId",
-                FieldType= FieldTypeEnum.Number,
-                ConnectRelation= ConnectRelationEnum.And,
-                NormalWhereRelation= NormalWhereRelationEnum.Equal,
-                WhereValue="1501290940000503"
-            },
-
-            new NormalFilterInfo(){
-                FieldName="PwdQuestion",
-                FieldType= FieldTypeEnum.Text,
-                ConnectRelation= ConnectRelationEnum.And,
-                NormalWhereRelation= NormalWhereRelationEnum.IsNotNull,
-            },
-
-            new NormalFilterInfo(){
-                FieldName="MobilePhone",
-                FieldType= FieldTypeEnum.Text,
-                ConnectRelation= ConnectRelationEnum.And,
-                NormalWhereRelation= NormalWhereRelationEnum.IsNull,
-            },
-
-            new NormalFilterInfo(){
-                FieldName="CreatedTime",
-                FieldType= FieldTypeEnum.DateTime,
-                ConnectRelation= ConnectRelationEnum.And,
-                NormalWhereRelation= NormalWhereRelationEnum.MoreThan,
-                WhereValue="2014-5-8 12:12:11"
-            },
-
-            };
-
-            string sql = _provider.BuildNormalWhere(list);
-
-        }
-
-
-
-        [TestMethod]
-        public void BuildCycleWhereTest()
-        {
-            var list = new List<CycleFilterInfo>() { 
-                new CycleFilterInfo(){
-                    FieldName="CreatedTime",
-                    ConnectRelation= ConnectRelationEnum.And,
-                    CycleRelation= CycleWhereRelationEnum.Month,
-                    MaxValue=10,
-                    MinValue=2
-                },
-                new CycleFilterInfo(){
-                    FieldName="CreatedTime",
-                    ConnectRelation= ConnectRelationEnum.And,
-                    CycleRelation= CycleWhereRelationEnum.Day,
-                    MaxValue=29,
-                    MinValue=14
-                },
-                new CycleFilterInfo(){
-                    FieldName="CreatedTime",
-                    ConnectRelation= ConnectRelationEnum.And,
-                    CycleRelation= CycleWhereRelationEnum.Week,
-                    MaxValue=7,
-                    MinValue=2
-                },
-                new CycleFilterInfo(){
-                    FieldName="CreatedTime",
-                    ConnectRelation= ConnectRelationEnum.And,
-                    CycleRelation= CycleWhereRelationEnum.Hour,
-                    MaxValue=20,
-                    MinValue=9
-                },
-            };
-            string sql = _provider.BuildCycleWhere(list);
-        }
-
-
-
+        /// <summary>
+        /// 测试构建完整where语句
+        /// </summary>
         [TestMethod]
         public void BuildWhereTest()
         {
@@ -177,6 +93,103 @@ namespace Mstm.SQLAnalysis.UnitTests
         }
 
 
+
+        /// <summary>
+        /// 测试构建标准的不带where的筛选语句
+        /// </summary>
+        [TestMethod]
+        public void BuildNormalWhereTest()
+        {
+            var list = new List<NormalFilterInfo>() { 
+            new NormalFilterInfo(){
+                FieldName="UserName",
+                FieldType= FieldTypeEnum.Text,
+                ConnectRelation= ConnectRelationEnum.Or,
+                NormalWhereRelation= NormalWhereRelationEnum.LikePrefix,
+                WhereValue="张"
+            },
+            new NormalFilterInfo(){
+                FieldName="UserId",
+                FieldType= FieldTypeEnum.Number,
+                ConnectRelation= ConnectRelationEnum.And,
+                NormalWhereRelation= NormalWhereRelationEnum.Equal,
+                WhereValue="1501290940000503"
+            },
+
+            new NormalFilterInfo(){
+                FieldName="PwdQuestion",
+                FieldType= FieldTypeEnum.Text,
+                ConnectRelation= ConnectRelationEnum.And,
+                NormalWhereRelation= NormalWhereRelationEnum.IsNotNull,
+            },
+
+            new NormalFilterInfo(){
+                FieldName="MobilePhone",
+                FieldType= FieldTypeEnum.Text,
+                ConnectRelation= ConnectRelationEnum.And,
+                NormalWhereRelation= NormalWhereRelationEnum.IsNull,
+            },
+
+            new NormalFilterInfo(){
+                FieldName="CreatedTime",
+                FieldType= FieldTypeEnum.DateTime,
+                ConnectRelation= ConnectRelationEnum.And,
+                NormalWhereRelation= NormalWhereRelationEnum.MoreThan,
+                WhereValue="2014-5-8 12:12:11"
+            },
+
+            };
+
+            string sql = _provider.BuildNormalWhere(list);
+
+        }
+
+
+
+        /// <summary>
+        /// 测试构建周期类型的不带where的筛选语句
+        /// </summary>
+        [TestMethod]
+        public void BuildCycleWhereTest()
+        {
+            var list = new List<CycleFilterInfo>() { 
+                new CycleFilterInfo(){
+                    FieldName="CreatedTime",
+                    ConnectRelation= ConnectRelationEnum.And,
+                    CycleRelation= CycleWhereRelationEnum.Month,
+                    MaxValue=10,
+                    MinValue=2
+                },
+                new CycleFilterInfo(){
+                    FieldName="CreatedTime",
+                    ConnectRelation= ConnectRelationEnum.And,
+                    CycleRelation= CycleWhereRelationEnum.Day,
+                    MaxValue=29,
+                    MinValue=14
+                },
+                new CycleFilterInfo(){
+                    FieldName="CreatedTime",
+                    ConnectRelation= ConnectRelationEnum.And,
+                    CycleRelation= CycleWhereRelationEnum.Week,
+                    MaxValue=7,
+                    MinValue=2
+                },
+                new CycleFilterInfo(){
+                    FieldName="CreatedTime",
+                    ConnectRelation= ConnectRelationEnum.And,
+                    CycleRelation= CycleWhereRelationEnum.Hour,
+                    MaxValue=20,
+                    MinValue=9
+                },
+            };
+            string sql = _provider.BuildCycleWhere(list);
+        }
+
+
+
+        /// <summary>
+        /// 测试构建时间点类型的不带where的筛选语句
+        /// </summary>
         [TestMethod]
         public void BuildPointInTimeWhereTest()
         {
@@ -199,6 +212,9 @@ namespace Mstm.SQLAnalysis.UnitTests
 
 
 
+        /// <summary>
+        /// 测试构建Max统计语句
+        /// </summary>
         [TestMethod]
         public void BuildStatisticsTest_Max()
         {
@@ -213,6 +229,9 @@ namespace Mstm.SQLAnalysis.UnitTests
 
 
 
+        /// <summary>
+        /// 测试构建Count统计语句
+        /// </summary>
         [TestMethod]
         public void BuildStatisticsTest_Count()
         {
@@ -222,6 +241,44 @@ namespace Mstm.SQLAnalysis.UnitTests
                 StatisticsRelation = StatisticsRelationEnum.Count
             };
             string sql = _provider.BuildStatistics(filter);
+        }
+
+
+        /// <summary>
+        /// 测试构建带统计的简单where语句
+        /// </summary>
+        [TestMethod]
+        public void BuildWhereWithStatisticsTest()
+        {
+            //筛选出用户Id大于平均值的,并且密保问题不为空的用户
+            StatisticsFilterInfo statisticsFilter = new StatisticsFilterInfo()
+            {
+                TableName = "UserInfo",
+                FieldName = "UserId",
+                StatisticsRelation = StatisticsRelationEnum.Avg
+            };
+
+            string avgSql = _provider.BuildStatistics(statisticsFilter);
+
+            var normalList = new List<NormalFilterInfo>() { 
+            new NormalFilterInfo(){
+                FieldName="UserId",
+                FieldType= FieldTypeEnum.Number,
+                ConnectRelation= ConnectRelationEnum.And,
+                NormalWhereRelation= NormalWhereRelationEnum.MoreThan,
+                WhereValue=avgSql
+            },
+            new NormalFilterInfo(){
+                FieldName="PwdQuestion",
+                FieldType= FieldTypeEnum.Text,
+                ConnectRelation= ConnectRelationEnum.And,
+                NormalWhereRelation= NormalWhereRelationEnum.IsNotNull,
+            }
+            };
+            FilterInfo filterInfo = new FilterInfo();
+            filterInfo.NormalFilterInfoList = normalList;
+
+            string sql = _provider.BuildWhere(filterInfo);
         }
     }
 }
