98f0a19cdc7624b9d5fc9ac6300b3e0848ae2021
mstm2014 <476256857@qq.com>
2016-03-18 16:28:47 +0800

    添加Mstm.Design.Core以及相应测试项目 该项目主要用于开发中通用的接口及抽象设计

---
+++ Design/Mstm.Design.Core/Factory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstm.Design.Core
+{
+    public abstract class Factory<TKey, TProvider> : IFactory<TKey, TProvider>
+    {
+
+
+        private static Dictionary<TKey, TProvider> _innerDict;
+
+
+        protected abstract Dictionary<TKey, TProvider> Init();
+
+
+        /// <summary>
+        /// 获取指定的Provider
+        /// </summary>
+        /// <param name="key">获取Provider的键</param>
+        /// <returns>TProvider实例，如果未获取到则返回默认值</returns>
+        public TProvider GetProvider(TKey key)
+        {
+            if (_innerDict == null)
+            {
+                _innerDict = Init();
+            }
+            if (key == null || _innerDict == null || _innerDict.ContainsKey(key) == false)
+            {
+                return default(TProvider);
+            }
+            else
+            {
+                return _innerDict[key];
+            }
+        }
+    }
+}
---
+++ Design/Mstm.Design.UnitTests/FactoryTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mstm.Design.Core;
+using System.Collections.Generic;
+using Shouldly;
+
+namespace Mstm.Design.UnitTests
+{
+    [TestClass]
+    public class FactoryTests
+    {
+        IFactory<string, FactoryImpl> _factory;
+
+
+        /// <summary>
+        /// 单个测试开始前
+        /// </summary>
+        [TestInitialize]
+        public void MethodInit()
+        {
+            _factory = new FactoryImpl();
+        }
+
+
+        /// <summary>
+        /// 单个测试结束后
+        /// </summary>
+        [TestCleanup]
+        public void MethodDispose()
+        {
+            _factory = null;
+        }
+
+
+        /// <summary>
+        /// 测试正常的输入
+        /// </summary>
+        [TestMethod]
+        public void GetProviderTest_NotNull()
+        {
+            var f1 = _factory.GetProvider("F1");
+            var f2 = _factory.GetProvider("F2");
+            var f3 = _factory.GetProvider("F3");
+            f1.ShouldNotBeNull();
+            f2.ShouldNotBeNull();
+            f3.ShouldNotBeNull();
+        }
+
+
+        /// <summary>
+        /// 测试未获取到指定键的情况
+        /// </summary>
+        [TestMethod]
+        public void GetProviderTest_IsNull()
+        {
+            IFactory<string, FactoryImpl> f6 = _factory.GetProvider("F6");
+            f6.ShouldBeNull<IFactory<string, FactoryImpl>>();
+        }
+
+
+        /// <summary>
+        /// 测试键为空的情况
+        /// </summary>
+        [TestMethod]
+        public void GetProviderTest_ArgIsNull()
+        {
+            IFactory<string, FactoryImpl> f6 = _factory.GetProvider(null);
+            f6.ShouldBeNull<IFactory<string, FactoryImpl>>();
+        }
+    }
+
+
+    class FactoryImpl : Factory<string, FactoryImpl>
+    {
+
+
+        protected override Dictionary<string, FactoryImpl> Init()
+        {
+            var dict = new Dictionary<string, FactoryImpl>()
+            {
+                {"F1",new FactoryImpl()},
+                {"F2",new FactoryImpl()},
+                {"F3",new FactoryImpl()},
+                {"F4",new FactoryImpl()},
+                {"F5",new FactoryImpl()},
+            };
+            return dict;
+        }
+    }
+}
