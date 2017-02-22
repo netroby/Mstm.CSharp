5371b59c9ac200925ec56c861a266beed40bbec1
mstmdev <476256857@qq.com>
2017-02-23 01:57:12 +0800

    添加Mstm.Common项目

---
+++ Common/Mstm.Common/Text/Base64Util.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstm.Common.Text
+{
+    /// <summary>
+    /// Base64相关工具类
+    /// </summary>
+    public class Base64Util
+    {
+        /// <summary>
+        /// 默认使用utf8作为编码
+        /// </summary>
+        public readonly static Encoding DefaultEncoding = Encoding.UTF8;
+
+        /// <summary>
+        /// 将字符串转化为Base64编码
+        /// </summary>
+        /// <param name="text">带编码的文本</param>
+        /// <param name="encoding">文本的编码格式，默认参见<see cref="Base64Util.DefaultEncoding"/></param>
+        /// <returns>Base64编码结果</returns>
+        public static string GetBase64String(string text, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = DefaultEncoding;
+            }
+            byte[] bytes = encoding.GetBytes(text);
+            string base64Str = Convert.ToBase64String(bytes);
+            return base64Str;
+        }
+
+        /// <summary>
+        /// 还原Base64编码文本
+        /// </summary>
+        /// <param name="base64Str">Base64编码文本</param>
+        /// <param name="encoding">文本的编码格式，默认参见<see cref="Base64Util.DefaultEncoding"/></param>
+        /// <returns>Base64编码的原始文本</returns>
+        public static string FromBase64String(string base64Str, Encoding encoding = null)
+        {
+            if (encoding == null)
+            {
+                encoding = DefaultEncoding;
+            }
+            byte[] bytes = Convert.FromBase64String(base64Str);
+            string text = encoding.GetString(bytes);
+            return text;
+        }
+    }
+}
---
+++ Common/Mstm.Common/Text/StringUtil.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstm.Common.Text
+{
+    public class StringUtil
+    {
+        public SecureString GetSecureString(string text)
+        {
+            SecureString secure = new SecureString();
+            var chars = text.ToCharArray();
+            chars.ToList().ForEach(c => secure.AppendChar(c));
+            return secure;
+        }
+
+        public string FromSecureString(SecureString secureStr)
+        {
+            string text = "";
+            unsafe
+            {
+                IntPtr ptr = new IntPtr();
+                try
+                {
+                    ptr = Marshal.SecureStringToCoTaskMemUnicode(secureStr);
+                    Char* pc = (Char*)ptr;
+
+                    for (int i = 0; pc[i] != 0; i++)
+                    {
+                        text += pc[i];
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                finally
+                {
+                    Marshal.ZeroFreeCoTaskMemUnicode(ptr);
+                }
+            }
+            return text;
+        }
+    }
+}
