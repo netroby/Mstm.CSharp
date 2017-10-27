e29e0970d46004577fb7961a334bc2cc9701ba87
mstmdev <476256857@qq.com>
2017-10-27 10:03:33 +0800

    添加ResultData类

---
+++ Common/Mstm.Common/Model/ResultData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mstm.Common.Model
+{
+    /// <summary>
+    /// 统一操作结果
+    /// </summary>
+    public class ResultData
+    {
+        /// <summary>
+        /// 服务是否出错
+        /// </summary>
+        public bool HasError { get; set; }
+
+        /// <summary>
+        /// 响应代码，0表示正常，小于0为服务出错代码，大于0为业务代码
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// 响应代码对应的提示消息
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// 服务返回的数据
+        /// </summary>
+        public object Data { get; set; }
+    }
+}
